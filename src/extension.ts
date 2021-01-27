aa5824863105377397fe6b0313a6818c56a83036
Nestor <elnestinatorHD@gmail.com>
2021-01-27 12:54:11 +0100

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,18 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { DoomPanel } from './DoomPanel';
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "vsdoom" is now active!');
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("vsdoom.run", () => {
+			DoomPanel.createOrShow(context.extensionUri);
+		})
+	);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
